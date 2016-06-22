8a4f1df874053d40ff9533d6809469167d82631e
Ian Berlot-Attwell <ian.berlot.attwell@mail.utoronto.ca>
2016-06-22 11:32:39 -0400

    Adds the .equals() method to SearchResult and SearchResults

--- src/main/java/edu/toronto/cs/se/ci/utils/searchEngine/SearchResult.java
+++ src/main/java/edu/toronto/cs/se/ci/utils/searchEngine/SearchResult.java
@@ -2,6 +2,7 @@ package edu.toronto.cs.se.ci.utils.searchEngine;
 
 /**
  * An object which holds one search result from an engine such as Google Search.
+ * 
  * @author wginsberg
  *
  */
@@ -10,7 +11,7 @@ public class SearchResult {
 	private String title;
 	private String link;
 	private String snippet;
-	
+
 	public SearchResult(String title, String link, String snippet) {
 		super();
 		this.title = title;
@@ -18,10 +19,10 @@ public class SearchResult {
 		this.snippet = snippet;
 	}
 
-	public SearchResult(){
+	public SearchResult() {
 		this("", "", "");
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
@@ -45,10 +46,20 @@ public class SearchResult {
 	public void setSnippet(String snippet) {
 		this.snippet = snippet;
 	}
-	
+
 	@Override
-	public String toString(){
+	public String toString() {
 		return String.format("%s\n%s\n%s\n", getTitle(), getSnippet(), getLink());
 	}
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof SearchResult)) {
+			return false;
+		}
+		SearchResult o = (SearchResult) obj;
+		return this.getLink().equals(o.getLink()) && this.getTitle().equals(o.getTitle())
+				&& this.getSnippet().equals(o.getSnippet());
+	}
+
 }
--- src/main/java/edu/toronto/cs/se/ci/utils/searchEngine/SearchResults.java
+++ src/main/java/edu/toronto/cs/se/ci/utils/searchEngine/SearchResults.java
@@ -3,26 +3,46 @@ package edu.toronto.cs.se.ci.utils.searchEngine;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
  * A class for holding a list of search results
+ * 
  * @author wginsberg
  *
  */
-public class SearchResults extends ArrayList<SearchResult>{
+public class SearchResults extends ArrayList<SearchResult> {
 
-	
 	public int hits;
 
-	public SearchResults(int hits, List<SearchResult> results){
+	public SearchResults(int hits, List<SearchResult> results) {
 		super(results);
 		this.hits = hits;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Search Results: " + hits + " Hits, and these results " + super.toString();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof SearchResults)) {
+			return false;
+		}
+		SearchResults o = (SearchResults) obj;
+		if (this.size() != o.size()) {
+			return false;
+		}
+		for (int x = 0; x < this.size(); x++) {
+			if (!this.get(x).equals(o.get(x))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	
 }
