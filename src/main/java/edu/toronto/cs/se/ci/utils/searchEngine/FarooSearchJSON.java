765a9ff927378e61f38992d381d1bd44697f56ac
Ian Berlot-Attwell <ian.berlot.attwell@mail.utoronto.ca>
2016-07-20 21:06:57 -0400

    Adds incomplete FAROO search

---
+++ src/main/java/edu/toronto/cs/se/ci/utils/searchEngine/FarooSearchJSON.java
@@ -0,0 +1,37 @@
+package edu.toronto.cs.se.ci.utils.searchEngine;
+
+import java.io.IOException;
+
+public class FarooSearchJSON implements JSONSearchEngine{
+
+	@Override
+	public SearchResults search(String searchString) throws IOException {
+		
+		return null;
+	}
+
+	@Override
+	public SearchResults search(String searchString, int pageNumber) throws IOException {
+		
+		return null;
+	}
+
+	@Override
+	public int getPageSize() {
+		
+		return 0;
+	}
+
+	@Override
+	public String getRawResults() {
+		
+		return null;
+	}
+
+	@Override
+	public SearchResults nextPage(SearchResults previousPage) throws IOException {
+		
+		return null;
+	}
+
+}
